12eee2327a719ea006fbc8f008a9f2c0a3635bb3
gitxuzan <gitxuzan@126.com>
2023-04-07 02:39:42 +0800

    新增责任链

--- znet/chain_test.go
+++ znet/chain_test.go
@@ -38,17 +38,9 @@ func Test_chain(t *testing.T) {
 	handlerA := &HandlerChainA{}
 	handlerB := &HandlerChainB{}
 	handlerC := &HandlerChainC{}
-	arr := []ziface.Chain{handlerA, handlerB, handlerC} // 按照这个顺序执行
-	lens := len(arr)
-	if lens > 0 {
-		for i, chain := range arr {
-			// 触发运行第一个链条，执行A 然后执行b
-			if i+1 < lens {
-				chain.SetNext(arr[i+1])
-			}
-		}
-		// 触发运行第一个链条，执行A 然后执行b , 再执行c
-		arr[0].HandleChainRequest(nil)
-	}
+	handlerA.SetNext(handlerB)
+	handlerB.SetNext(handlerC)
 
+	// 触发 依次 打印 A -> B -> C
+	handlerA.HandleChainRequest("")
 }
